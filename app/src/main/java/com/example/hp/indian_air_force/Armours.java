2b01387255bc3bcd84a597e579d8efaf5a0bdc94
guptavijay1 <vg7579@gmail.com>
2018-07-31 00:05:43 +0530

    armours activity done

--- app/src/main/java/com/example/hp/indian_air_force/Armours.java
+++ app/src/main/java/com/example/hp/indian_air_force/Armours.java
@@ -2,12 +2,57 @@ package com.example.hp.indian_air_force;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Armours extends AppCompatActivity {
 
+    TextView t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_armours);
+        t1=(TextView)findViewById(R.id.tv2);
+        t2=(TextView)findViewById(R.id.tv4);
+        t3=(TextView)findViewById(R.id.tv5);
+        t4=(TextView)findViewById(R.id.tv6);
+        t5=(TextView)findViewById(R.id.tv7);
+        t6=(TextView)findViewById(R.id.tv8);
+        t7=(TextView)findViewById(R.id.tv9);
+        t8=(TextView)findViewById(R.id.tv10);
+        t9=(TextView)findViewById(R.id.tv11);
+        t10=(TextView)findViewById(R.id.tv12);
+        t11=(TextView)findViewById(R.id.tv13);
+
+
+
+
+
+
+        t1.setText("Over the years the IAF has grown from a tactical force to one with transoceanic reach. The strategic reach emerges from induction of Force Multipliers like Flight Refuelling Aircraft (FRA), Remotely Piloted Aircraft (RPA) and credible strategic lift capabilities. There is emphasis on acquiring best of technology through acquisitions or upgradation, be it aircraft, systems, precision missiles or net centricity. The main inductions and acquisitions by Indian Air Force are given in the following paras.\n"+
+        "IAF has started upgrading its combat aircraft fleet since the last few years in order to enhance its operational capability and maintain its aircraft as modern weapon platforms, capable of meeting the present challenges posed by the security scenario in our region. Of the available fleet, MiG-21, MiG-27 and Jaguar aircraft have already been upgraded and Mirage-2000 and MiG-29 aircraft are planned for upgradation. The Indian Air Force is considering upgrade of its medium lift helicopters comprising Mi-8, Mi-17 and Mi-17-IVs, as also the AN - 32 transport aircraft, with the aim of improving their overall capability.");
+
+        t2.setText("The IAF's helicopter fleet has steadily increased in numbers over the past few years, blossoming from a handful of U.S. types in the 60s to over 500 French, Indian and Soviet built types. The pride of the force is, undoubtedly, the Mi-26 heavy lift helicopter which has been operated by No. 126 HU with outstanding results in the mountains of Northern India. The bulk of rotorcraft are Medium Lift Helicopters (MI-17/MI-17IV/MI-17V5 and Mi-8s) well over two hundred of these types serving in helicopter units through out the country, playing a vital logistic support role. Induction of the latest machine, the Mi-17 V5, is a quantum jump in our Medium Heli-lift capability in terms of the avionics, weapon systems as well as its hot and high altitude performance. Medium Lift Helicopters of IAF are operated for commando assault tasks, ferrying supplies and personnel to remote mountain helipads, carrying out SAR (Search and Rescue Operations) and logistic support tasks in the island territories, Siachen Glacier, apart from armed role");
+
+        t3.setText("Twin seater twin engine multirole fighter of Russian origin which carries One X 30mm GSH gun alongwith 8000 kg external armament. It is capable of carrying a variety of medium-range guided air to air missiles with active or semi-active radar or Infra red homing close range missiles. It has a max speed of 2500 km/hr");
+
+        t4.setText("A single seater air defence and multi-role fighter of French origin powered by a single engine can attain max speed of 2495 km/hr(Mach 2.3). It carries two 30 mm integral cannons and two matra super 530D medium-range and two R-550 magic II close combat missiles. It has very high max speed.");
+
+        t5.setText("Twin engine, single seater air superiority fighter aircraft of Russian origin capable of attaining max. speed of 2445 km per hour (Mach-2.3). It has a combat ceiling of 17 km. It carries a 30 mm cannon alongwith four R-60 close combat and two R-27 R medium range radar guided missiles");
+
+        t6.setText("Single engine, single seater tactical strike fighter aircraft of Russian origin having a max. speed of 1700 km/hr (Mach 1.6). It carries one 23 mm six-barrel rotary integral cannon and can carry upto 4000 kg of other armament externally.It has very high maximum speed.It has maintained a flawless accident free track record");
+
+        t7.setText("Single engine, single seater multirole fighter/ground attack aircraft of Russian origin which forms the back-bone of the IAF. It has a max speed of 2230 km/hr (Mach 2.1) and carries one 23mm twin barrel cannon with four R-60 close combat missiles.");
+
+        t8.setText("A twin-engine, single seater deep penetration strike aircraft of Anglo-French origin which has a max. speed of 1350 km /hr (Mach 1.3). It has two 30mm guns and can carry two R-350 Magic CCMs (overwing) alongwith 4750 kg of external stores (bombs/fuel). It has maintained a flawless accident free track record");
+
+        t9.setText("The aircraft is capable of carrying a payload of 40-70 tons up to a distance of 4200-9000 km in a single hop. The aircraft is capable of performing paradrop, heavy drop, casuality evacuation and can also operate from short and semi prepared surfaces. They are equipped with state-of-art navigational equipment");
+
+        t10.setText("Twin engine turboprop, medium tactical transport aircraft of Russian origin with a crew of five and capacity to carry 39 paratroopers or max load of 6.7 tonnes. It has a max cruise speed of 530 km/hr.They are equipped with state-of-art navigational equipment. It has maintained a flawless accident free track record.");
+
+        t11.setText("A four engine heavy duty/long haul military transport aircraft of Russian origin with a max speed of 850 km/hr. It has a twin 23 mm cannon in tail turret and capacity to carry 225 paratroopers or 40 tonnes freight, wheeled or tracked armoured vehicles.");
+
+
+
+
     }
 }
